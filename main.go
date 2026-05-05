1f7b6eb9247ee951ac682ca2c075676f67ec3fc2
Rörd Hinrichsen <roerdhh@gmail.com>
2026-05-05 22:29:17 +0200

    Use log instead of fmt for logging

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -27,7 +27,7 @@ func main() {
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
 	st, err := store.New(dataDir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create store: %v\n", err)
+		log.Printf("failed to create store: %v", err)
 		return 1
 	}
 	s := newServer(*st, httpPort, cancel)
@@ -36,21 +36,21 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 		serverErr = s.start()
 	}()
 
-	fmt.Printf("Linko is running on http://localhost:%d\n", httpPort)
+	log.Printf("Linko is running on http://localhost:%d", httpPort)
 
 	<-ctx.Done()
 
-	fmt.Println("Linko is shutting down")
+	log.Println("Linko is shutting down")
 
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to shutdown server: %v\n", err)
+		log.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
 	if serverErr != nil {
-		fmt.Fprintf(os.Stderr, "server error: %v\n", serverErr)
+		log.Printf("server error: %v", serverErr)
 		return 1
 	}
 	return 0
