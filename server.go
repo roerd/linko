d5e49b422905711baed1932938c89ed1f5d51c92
Rörd Hinrichsen <roerdhh@gmail.com>
2026-05-26 22:14:36 +0200

    Add profiling endpoints

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 
 	"boot.dev/linko/internal/store"
@@ -46,6 +47,9 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 	mux.HandleFunc("POST /admin/shutdown", s.handlerShutdown)
 
 	mux.Handle("GET /metrics", promhttp.Handler())
+
+	mux.Handle("GET /debug/pprof/", s.authMiddleware(http.HandlerFunc(pprof.Index)))
+	mux.Handle("GET /debug/pprof/profile", s.authMiddleware(http.HandlerFunc(pprof.Profile)))
 
 	return s
 }
